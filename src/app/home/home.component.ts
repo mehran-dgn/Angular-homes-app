472ca73cadb858ec8c643ebd25e77d5ee04b7fe0
mehran-dgn <mehrandehghanan@yahoo.com>
2024-09-02 15:16:23 +0330

    a simple service added / end of chapter 9 of tutorial

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component , inject} from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { Housinglocation } from '../housinglocation';
+import { NgFor } from '@angular/common';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocationComponent],
+  imports: [HousingLocationComponent,NgFor],
   template: `
   <section>
     <form>
@@ -14,23 +16,20 @@ import { Housinglocation } from '../housinglocation';
     </form>
   </section>
   <section class="results">
-    <app-housing-location [housingLocation] = "housingLocation"></app-housing-location>
+  <app-housing-location
+  *ngFor="let housingLocation of housingLocationList"
+  [housingLocation]="housingLocation">
+</app-housing-location>
   </section>
   `,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  housingLocationList :Housinglocation[] = [] ; 
+  housingService : HousingService = inject(HousingService);
 
-  housingLocation:Housinglocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
-  
+  constructor(){
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+
 }
