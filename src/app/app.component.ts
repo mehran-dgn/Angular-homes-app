dd1aae4190b07207c2bd8b33f41838ca4ab9eec3
mehran-dgn <mehrandehghanan@yahoo.com>
2024-09-03 00:27:40 +0330

    routing added / end of chapter 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HomeComponent],
+  imports: [RouterOutlet , HomeComponent , RouterModule],
   template: `
   <main>
     <header class="brand-name">
       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
     </header>
     <section class="content">
-      <app-home></app-home>
+    <router-outlet></router-outlet>
     </section>
   </main>
   `,
